2acaabee33e37c22b446a2bd9496b400beb5edbf
Denes <csaszar.denes@gmail.com>
2026-01-19 19:15:45 +0100

    performance optimization

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [],
+    formats: ["image/avif", "image/webp"],
   },
+  // Performance optimizations
+  swcMinify: true,
+  compress: true,
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig;
